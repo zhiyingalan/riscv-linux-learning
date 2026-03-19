d3296cf810cad17f72227a542b8d8de9de4c5b48
zhiyingalan <870776630@qq.com>
2026-03-20 00:36:34 +0800

    add lab code

---
+++ riscv_learning/lab_test/chapter3/benos/src/kernel.c
@@ -0,0 +1,24 @@
+#include "uart.h"
+
+extern void load_store_test(void);
+extern void pc_related_test(void);
+
+void asm_test(void)
+{
+	load_store_test();
+	pc_related_test();
+	my_memcpy();
+}
+
+void kernel_main(void)
+{
+	uart_init();
+	uart_send_string("Welcome RISC-V!\r\n");
+
+	asm_test();
+	uart_send_string("TEST done\r\n");
+
+	while (1) {
+		;
+	}
+}
