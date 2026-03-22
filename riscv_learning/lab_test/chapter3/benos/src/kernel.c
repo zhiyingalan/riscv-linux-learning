22ff675697731e4b18d99f9ba21d4a1ec9cea671
zhiyingalan <870776630@qq.com>
2026-03-22 16:47:02 +0800

    add compare asm function

--- riscv_learning/lab_test/chapter3/benos/src/kernel.c
+++ riscv_learning/lab_test/chapter3/benos/src/kernel.c
@@ -3,12 +3,26 @@
 extern void load_store_test(void);
 extern void pc_related_test(void);
 extern void my_memcpy(void);
+extern int compare_test(int a, int b);
 
 void asm_test(void)
 {
+	int ret = 0;
 	load_store_test();
 	pc_related_test();
 	my_memcpy();
+	ret = compare_test(1, 2);
+	if(ret==0) {
+		uart_send_string("compare_test(1, 2) OK!\r\n");
+	} else {
+		uart_send_string("compare_test(1, 2) FAIL!\r\n");
+	}
+	ret = compare_test(2, 1);
+	if(ret==0) {
+		uart_send_string("compare_test(2, 1) OK!\r\n");
+	} else {
+		uart_send_string("compare_test(2, 1) FAIL!\r\n");
+	}
 }
 
 void kernel_main(void)
