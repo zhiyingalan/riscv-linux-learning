9b6aba4610ebf756caf76590783abeb1af171b89
zhiyingalan <870776630@qq.com>
2026-03-22 17:18:42 +0800

    1.增加条件跳转测试 2.增加分支跳转测试

--- riscv_learning/lab_test/chapter3/benos/src/kernel.c
+++ riscv_learning/lab_test/chapter3/benos/src/kernel.c
@@ -4,6 +4,8 @@ extern void load_store_test(void);
 extern void pc_related_test(void);
 extern void my_memcpy(void);
 extern int compare_test(int a, int b);
+extern int compare_test2(int a, int b);
+extern int call_compare_test2(int a, int b);
 
 void asm_test(void)
 {
@@ -22,6 +24,26 @@ void asm_test(void)
 		uart_send_string("compare_test(2, 1) OK!\r\n");
 	} else {
 		uart_send_string("compare_test(2, 1) FAIL!\r\n");
+	}
+
+	ret = compare_test2(3, 5);
+	if(ret==7) {
+		uart_send_string("compare_test2(3, 5) return 7\n");
+	} else if(ret==4){
+		uart_send_string("compare_test2(3, 5) return 5\r\n");
+	}
+	ret = compare_test2(5, 2);
+	if(ret==4) {
+		uart_send_string("compare_test2(5, 3) return 4\n");
+	} else if(ret==1){
+		uart_send_string("compare_test2(5, 3) return 1\r\n");
+	}
+
+	ret = call_compare_test2(10, 20);
+	if(ret==22) {
+		uart_send_string("call_compare_test2(10, 20) return 22\n");
+	} else if(ret==19){
+		uart_send_string("call_compare_test2(10, 20) return 19\r\n");
 	}
 }
 
